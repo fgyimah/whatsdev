f7dda026350139bd880fcd506b7fd6efadef6694
Francis Gyimah <fgyimah883@gmail.com>
2019-08-23 22:22:39 +0000

    prepared for heroku deploy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -37,6 +38,17 @@ db.once("open", () => {
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  //get all urls for production mode
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 8989;
 
