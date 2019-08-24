527ff357c764c73d71932c63efcf9718563651eb
Francis Gyimah <fgyimah883@gmail.com>
2019-08-24 12:58:45 +0000

    heroku fixes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://localhost:5000/api`
+  baseURL: "http://localhost:" + process.env.PORT || `http://localhost:5000/api`
 });
